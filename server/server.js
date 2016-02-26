0c19521941d06c541e2e7e3962359ab927196817
Joshua Huang <jphuangjr@gmail.com>
2016-02-26 15:51:28 -0800

    skeleton file structure

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var mongoose = require("mongoose");
+
+/////////////////////////////////////////////////
+var databaseName = "temp" /*Database Name HERE*/
+////////////////////////////////////////////////
+
+var app = express();
+app.set('port', (process.env.PORT || 8000));
+
+// connect to mongo database named in databaseName
+//mongoose.connect('mongodb://localhost/'+ databaseName);
+
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+app.listen(app.get('port'), function(){
+	console.log("******* SERVER STARTED: Listening on port " + app.get('port') + " *******")
+});
+
+module.exports = app;
